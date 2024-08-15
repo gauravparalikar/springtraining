67153e2ef35147021201bc7a0777c21598055de9
Gaurav Paralikar <sp.gaurav@gmail.com>
2024-08-15 12:41:25 +0530

    changes in some files

--- src/main/java/com/ruhi/springboot/demo/springtraining/rest/HelloWorldController.java
+++ src/main/java/com/ruhi/springboot/demo/springtraining/rest/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.ruhi.springboot.demo.springtraining.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
 	// expose new rest api that return "Hello World"
+	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${coach.time}")
+	private String coachTime;
+	
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World!";
@@ -16,5 +26,16 @@ public class HelloWorldController {
 	@GetMapping("/goodbye")
 	public String sayGoodBye() {
 		return "Good Bye now!";
+	}
+	
+	@GetMapping("/coachdetails")
+	public String getCoachNamenTime() {
+		logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+        
+		return coachName+" ia available on "+coachTime;
 	}
 }
